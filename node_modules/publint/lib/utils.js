8666ab68da4ecd295ef5c0ad27f7bbaa8aa5822c
pfc <96251537+parallelqw@users.noreply.github.com>
2023-06-20 20:42:03 +0300

    Delete node_modules directory

--- node_modules/publint/lib/utils.js
+++
@@ -1,9 +0,0 @@
-export { formatMessagePath, getPkgPathValue } from '../src/utils.js'
-
-/**
- * no-op. leave users to implement themselves.
- * @type {import('../utils').printMessage}
- */
-export function printMessage() {
-  return
-}
