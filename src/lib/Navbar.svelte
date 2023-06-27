efb99443902f9aebcd5d413d4a50df15d11c9f90
Boranity <107348321+boranity@users.noreply.github.com>
2023-06-27 23:36:51 +0300

    redesigned to be responsive

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -70,4 +70,58 @@
     color: rgba(0, 255, 255, 0.726);
     font-weight: bold;
   }
+
+  @media only screen and (max-width: 768px) {
+    nav {
+    background-color: #252525;
+    padding: 13px;
+    display: flex;
+    justify-content: space-between;
+
+  }
+
+  .left-content {
+    display: flex;
+    align-items: center;
+    text-overflow: ellipsis;
+    font-size: 15px;
+
+
+  }
+  .middle-content {
+    display: grid;
+    margin-left: -2%;
+    justify-content: space-between;
+    text-overflow: ellipsis;
+    font-size: 15px;
+
+  }
+
+  .right-content {
+    display: flex;
+    align-items: center;
+    gap: 1px;
+    text-overflow: ellipsis;
+    font-size: 15px;
+  }
+
+  .nav-link {
+    text-decoration: none;
+    color: #22b9ff;
+    font-weight: bold;
+    margin-left: 10px;
+    transition: color 0.3s;
+    text-overflow: ellipsis;
+
+  }
+
+  .nav-link:hover {
+    color: #464746;
+  }
+
+  .logo-text {
+    color: rgba(0, 255, 255, 0.726);
+    font-weight: bold;
+  }
+  }
 </style>
